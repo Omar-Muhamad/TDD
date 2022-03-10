ed6d5e57c497345efc9120662421f2b97ea51a7e
kerolous samy <keroloussamy98@gmail.com>
2022-03-10 23:10:11 +0200

    Fix test and linters

--- solver_rspec.rb
+++ solver_rspec.rb
@@ -1,29 +1,38 @@
+require_relative './solver'
+
 describe 'Test solver class' do
   context 'Test factorial method' do
     it 'factorial method with positive numer' do
-      expect(factorial(5))to be(25)
+      solver = Solver.new
+      expect(solver.factorial(5)).to be(120)
     end
     it 'factorial method with Negative numer' do
-      expect(factorial(-5))to equal("Error: Your number should be positive.")
+      solver = Solver.new
+      expect { solver.factorial(-5) }.to raise_error('Error: Your number should be positive.')
     end
   end
   context 'Test Revese method' do
     it 'Revese the words of string' do
-      expect(reverse("omar"))to be("ramo")
+      solver = Solver.new
+      expect(solver.reverse('omar')).to eql('ramo')
     end
   end
   context 'Test FizzBuzz method' do
     it 'Shoud Return Fizz if the number dividable by 3' do
-      expect(fizz_buzz(9))to be("fizz")
+      solver = Solver.new
+      expect(solver.fizz_buzz(9)).to eql('fizz')
     end
     it 'Shoud Return buzz if the number dividable by 5' do
-      expect(fizz_buzz(5))to be("buzz")
+      solver = Solver.new
+      expect(solver.fizz_buzz(5)).to eql('buzz')
     end
     it 'Shoud Return FizzBuzz if the number dividable by 3 and 5' do
-      expect(fizz_buzz(15))to be("fizzbuzz")
+      solver = Solver.new
+      expect(solver.fizz_buzz(15)).to eql('fizzbuzz')
     end
     it 'Shoud Return the same number if the number not dividable by 3 and 5' do
-      expect(fizz_buzz(13))to be(13)
+      solver = Solver.new
+      expect(solver.fizz_buzz(13)).to be(13)
     end
   end
 end
